7e7958a99300e03c39b7c4fab3f75a00fbdfa5fd
jordanmsouza <jordan.msouza@outlook.com>
2021-05-10 17:30:38 -0300

    Ac4

--- app/model/tables.py
+++ app/model/tables.py
@@ -1,7 +1,7 @@
 from app import db
 # crio a tabela Aluno
 class Aluno(db.Model):
-    __tablename__ = "tb_Ac4_jordan2"
+    __tablename__ = "tb_Ac4_jordan3"
     ra = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(60))
     email = db.Column(db.String(50), unique=True)
