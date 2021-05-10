8a27141430a10e63fb334747e1893985b90b4d79
jordanmsouza <jordan.msouza@outlook.com>
2021-05-10 13:43:31 -0300

    Ac4

--- run.py
+++ run.py
@@ -6,8 +6,5 @@ if __name__== "__main__":
     db.create_all()
     # executa a aplicação
     app.run(debug=True)
-
-if _name_ == '_main_':
-    db.create_all()
     port = int(os.environ.get("PORT", 8000))
     app.run(host='0.0.0.0', port=port)
