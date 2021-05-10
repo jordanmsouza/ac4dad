26697bf9486a5831e9d72dab7caec37c67462707
jordanmsouza <jordan.msouza@outlook.com>
2021-05-10 13:40:47 -0300

    Ac4

--- run.py
+++ run.py
@@ -1,7 +1,13 @@
 from app import app, db
+import os
 
 if __name__== "__main__":
     #cria Banco
     db.create_all()
     # executa a aplicação
     app.run(debug=True)
+
+if _name_ == '_main_':
+    db.create_all()
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host='0.0.0.0', port=port)
